965932544d4f499eb0535eeca0601788f46ba168
Pawan singh <52067715+pawan-web@users.noreply.github.com>
2021-09-09 18:30:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Restraurent from "./components/basics/Restraurent";
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <Restraurent />;
+};
+
+export default App;
